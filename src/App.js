233219454f296412ec6e2ed7029a513b508529db
Matamala Daniel <daniel20mat@gmail.com>
2022-12-15 16:15:59 -0300

    adaptacion de los archivos para react 16

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { Banner } from "./components/Banner"
+import { Explanation } from "./components/Explanation"
+import { Presentation } from "./components/Presentation"
+import { StickyFooter } from "./components/StickyFooter"
+import { Layout } from "./components/Layout"
+import { ThemeProvider} from "@material-ui/core"
+import { createTheme } from '@material-ui/core/styles'
 
 function App() {
+  const theme = createTheme({
+    breakpoints: {
+      values: {
+        xxs: 320,
+        xs: 480,
+        sm: 640,
+        md: 768,
+        lg: 1024,
+        xl: 1220,
+        xxl: 1366,
+        xxxl: 1620,
+      }
+    }
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ThemeProvider theme={theme}>
+      <Layout>
+        <Banner />
+        <Explanation />
+        <Presentation />
+        <StickyFooter />
+      </Layout>
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App

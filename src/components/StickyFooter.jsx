e3b5b96009cd3b5c00df6504ad601c552753a317
Matamala Daniel <daniel20mat@gmail.com>
2022-12-19 15:47:13 -0300

    completado de stickyFooter

--- src/components/StickyFooter.jsx
+++ src/components/StickyFooter.jsx
@@ -1,16 +1,46 @@
 import React from 'react'
-// import { makeStyles } from '@material-ui/core'
+import { Container, makeStyles, Typography, Link } from '@material-ui/core'
+import FavoriteIcon from '@material-ui/icons/Favorite'
 
-export const StickyFooter = () => {
-  // const classes = useStyles();
+const Copyright= () => {
   return (
-    <div>Pie de pagina</div>
+    <Typography variant='body2' color='textSecondary'>
+      Copyright©️
+      <Link color='inherit' href='https://google.com'
+            target="_blank" rel="norereferrer">
+            pagina-de-prueba.com</Link>
+            {
+              new Date().getFullYear()
+            }
+    </Typography>
   )
 }
 
-// const useStyles = makeStyles(({ theme }) => ({
-//     root:{
-      
-      
-//   }
-// }))
+export const StickyFooter = () => {
+  const classes = useStyles()
+  return (
+    <footer className={classes.footer}>
+      <Container maxWidth="sm">
+          <Typography variant='body1'>
+              Curate with <FavoriteIcon style={{color:"red"}}/>
+              by the <span>
+                <a href="https://google.com"
+                    target="_blank"
+                    rel="noreferrer">
+                  Lorem ipsum dolor
+                </a>
+              </span>
+          </Typography>
+          <Copyright />
+      </Container>
+    </footer>
+  )
+}
+
+const useStyles = makeStyles((theme) => ({
+  footer: {
+    padding: theme.spacing(3, 2),
+    marginTop: "auto",
+    textAlign: "center"
+  }
+}))

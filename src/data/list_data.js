7f9df4c3699d38a6f227e4cc3fe3fd310dc1d3d5
Matamala Daniel <daniel20mat@gmail.com>
2022-12-19 15:03:56 -0300

    carga de datos de prueba

---
+++ src/data/list_data.js
@@ -0,0 +1,50 @@
+import AccessibleIcon from '@material-ui/icons/Accessible';
+import AccessibleForwardIcon from '@material-ui/icons/AccessibleForward';
+import AirlineSeatReclineExtraIcon from '@material-ui/icons/AirlineSeatReclineExtra';
+import AirlineSeatReclineNormalIcon from '@material-ui/icons/AirlineSeatReclineNormal';
+import AirlineSeatFlatIcon from '@material-ui/icons/AirlineSeatFlat';
+import EmojiPeopleIcon from '@material-ui/icons/EmojiPeople';
+import ExtensionIcon from '@material-ui/icons/Extension';
+import FlightIcon from '@material-ui/icons/Flight';
+import GitHubIcon from '@material-ui/icons/GitHub';
+
+const listData = [
+    {
+        text:"'classes' is assigned a value but never used",
+        icon:<AccessibleIcon/>
+    },
+    {
+        text:"'classes' is assigned a value but never used",
+        icon:<AccessibleForwardIcon/>
+    },
+    {
+        text:"'classes' is assigned a value but never used",
+        icon:<AirlineSeatReclineExtraIcon/>
+    },
+    {
+        text:"'classes' is assigned a value but never used",
+        icon:<AirlineSeatReclineNormalIcon/>
+    },
+    {
+        text:"'classes' is assigned a value but never used",
+        icon:<EmojiPeopleIcon/>
+    },
+    {
+        text:"'classes' is assigned a value but never used",
+        icon:<AirlineSeatFlatIcon/>
+    },
+    {
+        text:"'classes' is assigned a value but never used",
+        icon:<ExtensionIcon/>
+    },
+    {
+        text:"'classes' is assigned a value but never used",
+        icon:<FlightIcon/>
+    },
+    {
+        text:"'classes' is assigned a value but never used",
+        icon:<GitHubIcon/>
+    },
+]
+export default listData
+
---
+++ src/data/tile_data.js
@@ -0,0 +1,53 @@
+import foto from "../images/paisaje.jpeg"
+
+const tile_data = [
+    {
+        image: foto,
+        title: "Paisaje Natural"
+    },
+    {
+        image: foto,
+        title: "Paisaje Natural"
+    },
+    {
+        image: foto,
+        title: "Paisaje Natural"
+    },
+    {
+        image: foto,
+        title: "Paisaje Natural"
+    },
+    {
+        image: foto,
+        title: "Paisaje Natural"
+    },
+    {
+        image: foto,
+        title: "Paisaje Natural"
+    },
+    {
+        image: foto,
+        title: "Paisaje Natural"
+    },
+    {
+        image: foto,
+        title: "Paisaje Natural"
+    },
+    {
+        image: foto,
+        title: "Paisaje Natural"
+    },
+    {
+        image: foto,
+        title: "Paisaje Natural"
+    },
+    {
+        image: foto,
+        title: "Paisaje Natural"
+    },
+    {
+        image: foto,
+        title: "Paisaje Natural"
+    },
+]
+export default tile_data

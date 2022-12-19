63ca0dcab1b070d962c97d102be05f987d90699e
Matamala Daniel <daniel20mat@gmail.com>
2022-12-19 15:17:11 -0300

    estilando la presentacion

--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
-// import { makeStyles } from '@material-ui/core'
+import { makeStyles, Paper, Typography } from '@material-ui/core'
+import ListGallery from "./ListGallery"
+
 export const Presentation = () => {
-  // const classes = useStyles();
+  const classes = useStyles()
   return (
-    <div>Presentation</div>
+    <Paper variant='outlined' square elevation={0} className={classes.presentation}>
+      <Typography variant='h5'>
+        Our package includes
+      </Typography>
+      <ListGallery />
+    </Paper>
   )
 }
 
-// const useStyles = makeStyles(({ theme }) => ({
-//     root:{
-     
-//   }
-// }))
+const useStyles = makeStyles( theme  => ({
+  presentation: {
+    width: "inherit",
+    height: "auto",
+    padding: theme.spacing(2)
+  }
+}))
